6731aa87eff24696aed40b9d21f40d5fc5b949d4
arora <arora.geeks@gmail.com>
2024-04-20 22:49:20 +0530

    changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ConnectionTransport, ConnectionTransports, IConnectionOptions, SignalR } from 'ng2-signalr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webngframesignalr';
+  
+
+
+
+  /**
+   *
+   */
+  constructor(private _signalR: SignalR) {
+    this.someFunction();
+    
+  }
+
+  someFunction() {
+    var options: IConnectionOptions = {
+       hubName: 'ChatSampleHub'};
+       //transport:[ConnectionTransports.webSockets, ConnectionTransports.longPolling] };
+    // maybe hub details needed
+    this._signalR.connect(options).then((c) => {
+      //do stuff
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SignalRModule } from 'ng2-signalr';
+import { SignalRConfiguration } from 'ng2-signalr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+export function createConfig(): SignalRConfiguration {
+  const c = new SignalRConfiguration();
+  c.hubName = 'Ng2SignalRHub';
+  c.qs = { user: 'donald' };
+  c.url = 'https://localhost:44355/';
+  c.logging = true;
+  
+  // >= v5.0.0
+  c.executeEventsInZone = true; // optional, default is true
+  c.executeErrorsInZone = false; // optional, default is false
+  c.executeStatusChangeInZone = true; // optional, default is true
+  return c;
+}
